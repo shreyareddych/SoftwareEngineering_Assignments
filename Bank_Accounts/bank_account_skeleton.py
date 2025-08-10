062224daba00740ddf4a9dbc45ce2e5c52a756e3
Shreya <chidiralashreyareddy1111@gmail.com>
2025-08-10 05:13:13 -0400

    Add Bank_Accounts folder with Q1 and Q2 skeletons

---
+++ Bank_Accounts/bank_account_skeleton.py
@@ -0,0 +1,23 @@
+# TODO: Replace <your_name> with your name before submission.
+# This is the skeleton for BankAccount assignment.
+
+class BankAccount:
+    # TODO: Class attribute for bank title
+
+    def __init__(self, customer_name, current_balance, minimum_balance):
+        # TODO: Initialize instance attributes
+        pass
+
+    def deposit(self, amount):
+        # TODO: Implement deposit logic
+        pass
+
+    def withdraw(self, amount):
+        # TODO: Implement withdrawal logic with minimum balance check
+        pass
+
+    def print_customer_information(self):
+        # TODO: Print customer details including bank title
+        pass
+
+# TODO: Create at least two instances to test
---
+++ Bank_Accounts/main_skeleton.py
@@ -0,0 +1,8 @@
+from bank_account_skeleton import BankAccount
+
+def main():
+    # TODO: Create 2 BankAccount objects and call methods
+    pass
+
+if __name__ == "__main__":
+    main()
