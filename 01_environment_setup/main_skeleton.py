1e832b62204d00f641d29f913c23d1cb468d9ff2
Shreya <chidiralashreyareddy1111@gmail.com>
2025-08-10 04:47:09 -0400

    Add skeleton code for 01_environment_setup

---
+++ 01_environment_setup/main_skeleton.py
@@ -0,0 +1,9 @@
+# main_skeleton.py
+# TODO: Replace <your_name> with your actual name before running
+
+def main():
+    # TODO: Print "Welcome <your_name> to the Software Engineering course!"
+    pass
+
+if __name__ == "__main__":
+    main()
